7a2e223d88c6f4f10194c9cc50cdef5d40cec471
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2016-03-09 13:32:49 -0500

    [ INITIAL ] release

---
+++ README.md
@@ -0,0 +1,120 @@
+### React-Native-Torrent-Streamer
+
+> Torrent Streamer for react-native
+
+*Only Android support now.*
+
+#### Integrate
+
+##### Android
+
+* Install via npm
+`npm i react-native-torrent-streamer --save-dev`
+
+* Add dependency to `android/settings.gradle`
+```
+...
+include ':react-native-torrent-streamer'
+project(':react-native-torrent-streamer').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-torrent-streamer/android/torrent')
+```
+
+* Add `android/app/build.gradle`
+```
+...
+dependencies {
+    ...
+    compile project(':react-native-torrent-streamer')
+}
+```
+* Register module in `MainActivity.java`
+```
+import com.ghondar.torrentstreamer.*;  // <--- import
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new TorrentStreamerPackage())  // <------- here
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "doubanbook", null);
+
+        setContentView(mReactRootView);
+    }
+```
+
+#### Usage
+```
+var RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
+var Subscribable = require('Subscribable');
+var TorrentStreamer = require('react-native-torrent-streamer');
+
+
+var doubanbook = React.createClass({
+
+  mixins: [Subscribable.Mixin],
+
+  componentWillMount: function() {
+    this.addListenerOn(RCTDeviceEventEmitter, 'error', this.onError)
+    this.addListenerOn(RCTDeviceEventEmitter, 'progress', this.onProgress.bind(this))
+    this.addListenerOn(RCTDeviceEventEmitter, 'ready', this.onReady.bind(this))
+    this.addListenerOn(RCTDeviceEventEmitter, 'stop', this.onStop.bind(this))
+  },
+
+  onError: function(err) {
+    console.log(err)
+  },
+
+  onProgress: function(progress) {
+    console.log(progress.data)
+  },
+
+  onReady: function(data) {
+    console.log("onReady")
+    TorrentStreamer.open(data.url)
+  },
+
+  onStop: function() {
+    console.log("onStop")
+  },
+
+  start: function() {
+    TorrentStreamer.start('magnet:?xt=urn:btih:D60795899F8488E7E489BA642DEFBCE1B23C9DA0&dn=Kingsman%3A+The+Secret+Service+%282014%29+%5B720p%5D&tr=http%3A%2F%2Ftracker.yify-torrents.com%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.org%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337')
+  },
+
+  buttonClicked: function() {
+    TorrentStreamer.prepare()
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.buttonClicked}>
+            <Text >Prepare!</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.start}>
+            <Text >Start Torrent!</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  }
+})
+
+```
+
+#### LICENSE
+MIT
---
+++ android/torrent/src/main/java/com/ghondar/torrentstreamer/TorrentStreamer.java
@@ -0,0 +1,120 @@
+package com.nativemodule;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.github.sv244.torrentstream.StreamStatus;
+import com.github.sv244.torrentstream.Torrent;
+import com.github.sv244.torrentstream.TorrentOptions;
+import com.github.sv244.torrentstream.TorrentStream;
+import com.github.sv244.torrentstream.listeners.TorrentListener;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class TorrentStreamer extends ReactContextBaseJavaModule implements TorrentListener {
+
+    private TorrentStream mTorrentStream;
+    private ReactApplicationContext context;
+
+    public TorrentStreamer(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.context = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "TorrentStreamer";
+    }
+
+    @ReactMethod
+    public void prepare() {
+        TorrentOptions torrentOptions = new TorrentOptions();
+        torrentOptions.setSaveLocation(this.context.getExternalCacheDir());
+        torrentOptions.setMaxConnections(200);
+        torrentOptions.setMaxDownloadSpeed(0);
+        torrentOptions.setMaxUploadSpeed(0);
+        torrentOptions.setRemoveFilesAfterStop(true);
+
+        mTorrentStream = TorrentStream.init(torrentOptions);
+        mTorrentStream.addListener(this);
+    }
+
+    @ReactMethod
+    public void start(String magnetUrl) {
+        mTorrentStream.startStream(magnetUrl);
+    }
+
+    private void sendEvent(String eventName,
+                           @Nullable WritableMap params) {
+        this.context
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    @Override
+    public void onStreamPrepared(Torrent torrent) {
+        WritableMap params = Arguments.createMap();
+        params.putString("data", "OnStreamPrepared");
+        sendEvent("progress", params);
+        torrent.startDownload();
+    }
+
+    @Override
+    public void onStreamStarted(Torrent torrent) {
+        WritableMap params = Arguments.createMap();
+        params.putString("data", "onStreamStarted");
+        sendEvent("progress", params);
+    }
+
+    @Override
+    public void onStreamError(Torrent torrent, Exception e) {
+        WritableMap params = Arguments.createMap();
+        params.putString("msg", e.getMessage());
+        sendEvent("error", params);
+    }
+
+    @Override
+    public void onStreamReady(Torrent torrent) {
+        WritableMap params = Arguments.createMap();
+        params.putString("url", torrent.getVideoFile().toString());
+        sendEvent("ready", params);
+    }
+
+    @Override
+    public void onStreamProgress(Torrent torrent, StreamStatus status) {
+
+        if(status.bufferProgress <= 100) {
+            WritableMap params = Arguments.createMap();
+            params.putString("data", ""+status.bufferProgress);
+            sendEvent("progress", params);
+        }
+    }
+
+    @Override
+    public void onStreamStopped() {
+        WritableMap params = Arguments.createMap();
+        params.putString("msg", 'OnStreamStoped');
+        sendEvent("stop", params);
+    }
+
+    @ReactMethod
+    public void open(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(url), "video/mp4");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Check that an app exists to receive the intent
+        if (intent.resolveActivity(this.context.getPackageManager()) != null) {
+            this.context.startActivity(intent);
+        }
+    }
+}
